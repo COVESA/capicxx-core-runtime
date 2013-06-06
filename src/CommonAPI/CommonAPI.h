f03d00894cae91ccf7e4f3121f6eaec941682389
Philip Rauwolf <rauwolf@itestra.de>
2013-06-06 13:16:16 +0200

    Added CommonAPI.h for general usage in user code.

---
+++ src/CommonAPI/CommonAPI.h
@@ -0,0 +1,22 @@
+/* Copyright (C) 2013 BMW Group
+ * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
+ * Author: Juergen Gehring (juergen.gehring@bmw.de)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef COMMONAPI_H_
+#define COMMONAPI_H_
+
+
+#define COMMONAPI_COMPILATION
+
+#include "Runtime.h"
+#include "Factory.h"
+#include "AttributeExtension.h"
+#include "types.h"
+
+#undef COMMONAPI_COMPILATION
+
+
+#endif /* COMMONAPI_H_ */
