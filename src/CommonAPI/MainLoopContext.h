6d0f3cfe4b7a0db3eabb372ec44e0c0c6706aa5f
Philip Rauwolf <rauwolf@itestra.de>
2013-10-16 10:38:22 +0200

    Changed formerly static function to inline

--- src/CommonAPI/MainLoopContext.h
+++ src/CommonAPI/MainLoopContext.h
@@ -33,7 +33,7 @@ enum class DispatchPriority {
 };
 
 
-static int64_t getCurrentTimeInMs() {
+inline int64_t getCurrentTimeInMs() {
    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 }
 
